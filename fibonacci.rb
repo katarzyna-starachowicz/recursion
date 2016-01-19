8c7015c730ca03fa893e72b366e2ab715d6cf76d
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-01-20 00:18:35 +0100

    plus one method shorter but stolen

--- fibonacci.rb
+++ fibonacci.rb
@@ -30,6 +30,13 @@ def fibs_rec(n, numbers = [], a = 0, b = 1)
   fibs_rec(n - 1, numbers, a, b)
 end
 
+#shorter but stolen from: https://github.com/RobotOptimist/rec_ex/blob/master/fibs.rb
+def fibs_rec_stolen(n, x = 1, y = 2, fib = [1])
+  return fib if fib.size >= n
+  fib << y
+  fibs_rec_stolen(n, fib[-1], (fib[-1] + x), fib)
+end
+
 # only n!
 def fib_rec(n)
   if n <= 0
