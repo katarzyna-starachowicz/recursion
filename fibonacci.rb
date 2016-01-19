17c2ae9d8e2815bab6f6e946c85405e99cf1b7b4
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-01-20 00:13:14 +0100

    fibonacci with recursion

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,17 +1,42 @@
-def fibs(n) # with iteration
+# Write a method #fibs which takes a number 
+# and returns that many members of the fibonacci sequence. 
+# Use iteration for this solution.
+
+def fibs(n)
   a = 0
   b = 1
-  numbers = [0, 1]
-  (n - 2).times do
+  numbers = [1]
+  (n - 1).times do
     b = a + b
     a = b - a
     numbers << b
   end
-  if n == 1
-    0
-  elsif n < 1
-    "n must be bigger than 0"    
+  if n < 1
+    "n must be greater than 0"    
   else
     numbers
+  end
+end
+
+# --------------------------------------------
+# Now write another method #fibs_rec 
+# which solves the same problem recursively.
+
+def fibs_rec(n, numbers = [], a = 0, b = 1)
+  return (numbers << [1] * n) if n <= 0
+  numbers << b
+  b = a + b
+  a = b - a
+  fibs_rec(n - 1, numbers, a, b)
+end
+
+# only n!
+def fib_rec(n)
+  if n <= 0
+    return "n must be greater than 0"  
+  elsif n == 1
+    return 1
+  else    
+    fib_rec(n - 1) + fib_rec(n - 2)
   end
 end
